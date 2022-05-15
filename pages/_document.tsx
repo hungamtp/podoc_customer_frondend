a08fd7168ded7c67889ae48ddf4221f21079ef2d
Hung <hungamtp@gmail.com>
2022-05-15 09:24:56 +0700

    avoid =eslint in document ile

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,37 +1,10 @@
+/* eslint-disable @next/next/no-sync-scripts */
+/* eslint-disable @next/next/no-css-tags */
+/* eslint-disable @next/next/no-title-in-document-head */
 import React, { ReactElement } from 'react';
 import Document, { Html, Head, Main, NextScript, DocumentInitialProps, DocumentContext } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
-
-// NEXT.JS CUSTOM DOCUMENT
-// https://nextjs.org/docs/advanced-features/custom-document
 
 export default class MyDocument extends Document {
-  // static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-  //   const sheet = new ServerStyleSheet();
-  //   const originalRenderPage = ctx.renderPage;
-
-  //   try {
-  //     ctx.renderPage = () =>
-  //     originalRenderPage({
-  //       enhanceApp: (App) => (props) =>
-  //         sheet.collectStyles(<App {...props} />),
-  //     });
-
-  //     const initialProps = await Document.getInitialProps(ctx);
-  //     return {
-  //       ...initialProps,
-  //       styles: (
-  //         <>
-  //           {initialProps.styles}
-  //           {sheet.getStyleElement()}
-  //         </>
-  //       ),
-  //     };
-  //   } finally {
-  //     sheet.seal();
-  //   }
-  // }
-
   render(): ReactElement {
     return (
       <Html lang="en">
