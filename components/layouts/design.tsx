b5b849ad07d050fe0facf729abbc74ef7c1f0ea9
studingHtml <hieuthomnghiep@gmail.com>
2022-05-22 22:36:20 +0700

    update left part

---
+++ components/layouts/design.tsx
@@ -0,0 +1,10 @@
+import { LayoutProps } from "@/models/index";
+import Link from "next/link";
+import * as React from "react";
+
+export interface IDesignLayouyProps {}
+
+export function DesignLayouy({ children }: LayoutProps) {
+  return;
+  <div>{children}</div>;
+}
