2b723b7d75a9788c3c3a71d54c7af7a26d9943e5
Sang <vuvietsang10a9@gmail.com>
2022-06-11 15:18:53 +0700

    update factory component

--- components/common/factory.tsx
+++ components/common/factory.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react';
-import { useRouter } from 'next/router';
-import { FactoryDTO } from '@/services/type.dto';
+import React from "react";
+import { useRouter } from "next/router";
+import { FactoryDTO } from "@/services/type.dto";
 
 type Props = {
   factory: FactoryDTO;
@@ -9,7 +9,7 @@ type Props = {
 export default function Factory({ factory }: Props) {
   const router = useRouter();
   const startDesign = () => {
-    router.push('/design');
+    router.push("/design");
   };
 
   return (
@@ -18,15 +18,17 @@ export default function Factory({ factory }: Props) {
         <div className="card">
           <div className="card-header card-factory-header">
             <div className="right-card-factory-header">
-              <span style={{ marginRight: ' 20px' }}>
+              <span style={{ marginRight: " 20px" }}>
                 <b className="mb-0"> {factory.name}</b>
               </span>
-              <span className="mb-0">{factory.location}</span>
             </div>
 
             <div className="left-card-factory-header">
               <span>
-                <button className="btn btn-primary btn-sm" onClick={startDesign}>
+                <button
+                  className="btn btn-primary btn-sm"
+                  onClick={startDesign}
+                >
                   Start Design
                 </button>
               </span>
@@ -35,46 +37,50 @@ export default function Factory({ factory }: Props) {
           <div className="card-body">
             <div className="card-factory-body">
               <div className="card-factory-body-content">
-                <div className="small-text text-muted mb-0 card-body-title">Price</div>
+                <div className="small-text text-muted mb-0 card-body-title">
+                  Location
+                </div>
+                <div>
+                  <i className="bi bi-geo-alt-fill text-info"></i>
+                  <span className="mb-0">{factory.location}</span>
+                </div>
+              </div>
+              <div className="card-factory-body-content">
+                <div className="small-text text-muted mb-0 card-body-title">
+                  Price
+                </div>
                 <div>{factory.price}</div>
               </div>
               <div>
-                <div className="small-text text-muted mb-0 card-body-title">Size</div>
-                <div>{factory.sizes.join(' , ')}</div>
+                <div className="small-text text-muted mb-0 card-body-title">
+                  Size
+                </div>
+                <div>{factory.sizes.join(" , ")}</div>
               </div>
               <div>
-                <div className="small-text text-muted mb-0 card-body-title">Area</div>
-                <div>{factory.area.join(' , ')}</div>
+                <div className="small-text text-muted mb-0 card-body-title">
+                  Area
+                </div>
+                <div>{factory.area.join(" , ")}</div>
               </div>
               <div>
-                <div className="small-text card-body-title">Color</div>
-                <ul className="list-unstyled mt-4 mb-0">
-                  <li className="list-inline-item">
-                    <a className="btn btn-sm btn-icon btn-pills btn-primary">
-                      <span className="d-none">.</span>
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a className="btn btn-sm btn-icon btn-pills btn-danger">
-                      <span className="d-none">.</span>
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a className="btn btn-sm btn-icon btn-pills btn-success">
-                      <span className="d-none">.</span>
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a className="btn btn-sm btn-icon btn-pills btn-info">
-                      <span className="d-none">.</span>
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a className="btn btn-sm btn-icon btn-pills btn-secondary">
-                      <span className="d-none">.</span>
-                    </a>
-                  </li>
-                </ul>
+                <div className="small-text mb-0 card-body-title">Color</div>
+                <div>
+                  <ul className="list-unstyled mb-0">
+                    <li className="list-inline-item">
+                      {factory.colors.map((color) => (
+                        <img
+                          key={color}
+                          width={22}
+                          height={22}
+                          className="rounded-circle"
+                          src={color}
+                          alt={color}
+                        />
+                      ))}
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
