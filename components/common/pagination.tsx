eea2d118018e2377b049e0fcd9e51e7aaa540c84
Hung <hungamtp@gmail.com>
2022-06-12 07:39:34 +0700

    Render pagination

--- components/common/pagination.tsx
+++ components/common/pagination.tsx
@@ -1,59 +1,35 @@
-import { type } from 'os';
 import React, { useState } from 'react';
 type Props = {
   currentPage: number;
   pages: number;
 };
-function renderPage(pages: number, currentPage: number) {
-  let page = [];
-
-  for (var i = 1; i <= pages; i++) {
-    page.push(
-      <li key={i} className={i == currentPage ? 'page-item' : 'page-item active'}>
-        <a className="page-link" href=" ">
-          {i}
-        </a>
-      </li>
-    );
-    return page;
-  }
-}
 
 export default function Pagination({ currentPage, pages }: Props) {
-  const [page, setPage] = useState(currentPage);
-  // fucntion renderPage(pages): number[]  {
-  //   let page = [];
-  //   for (let i = 1; i <= pages; i++) {
-  //     page.push(i);
-  //   }
-  //   return page;
-  // };
+  const numbers = (pages: number): number[] => {
+    let result = [];
+    for (var i = 1; i <= pages; i++) {
+      result.push(i);
+    }
+    return result;
+  };
   return (
     <div className="col-12 mt-4 pt-2">
       <ul className="pagination justify-content-center mb-0">
         <li className={currentPage == 0 ? 'page-item disabled' : 'page-item'}>
-          <a className="page-link" href=" " aria-label="Previous">
+          <span className="page-link" aria-label="Previous">
             <i className="mdi mdi-arrow-left" /> Prev
-          </a>
+          </span>
         </li>
-        {renderPage(pages, currentPage)}
-        {/* <li className="page-item active">
-          <a className="page-link" href=" ">
-            1
-          </a>
-        </li>
-        <li className="page-item">
-          <a className="page-link" href=" ">
-            2
-          </a>
-        </li>
-        <li className="page-item">
-          <a className="page-link" href=" ">
-            3
-          </a>
-        </li> */}
-        <li className={currentPage == pages ? 'page-item disabled' : 'page-item'}>
-          <a className="page-link" href=" " aria-label="Next">
+        {numbers(pages).map(page => {
+          return (
+            <li key={page} className={`page-item ${page == currentPage + 1 && 'active'} `}>
+              <span className="page-link">{page}</span>
+            </li>
+          );
+        })}
+
+        <li className={currentPage == pages - 1 ? 'page-item disabled' : 'page-item'}>
+          <a className="page-link" aria-label="Next">
             Next <i className="mdi mdi-arrow-right" />
           </a>
         </li>
