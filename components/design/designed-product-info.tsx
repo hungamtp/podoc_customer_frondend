7cc247c67ebaed5696a5988f8ebad45a5337dd9c
studingHtml <hieuthomnghiep@gmail.com>
2022-06-17 23:25:03 +0700

    update preview

--- components/design/designed-product-info.tsx
+++ components/design/designed-product-info.tsx
@@ -7,7 +7,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import FormControl from "@mui/material/FormControl";
 import MenuItem from "@mui/material/MenuItem";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
-import { Theme, useTheme } from "@mui/material/styles";
+
 import {
   getDownloadURL,
   ref,
@@ -20,6 +20,7 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { ImageListType } from "react-images-uploading";
 import * as yup from "yup";
 import { useAppSelector } from "../hooks/reduxHook";
+import SelectColor from "./select-color";
 
 export interface ICreateDesignedProductFormProps {
   handleCloseDialog: () => void;
@@ -54,36 +55,13 @@ function b64toBlob(dataURI: string) {
   return new Blob([ab], { type: "image/jpeg" });
 }
 
-function getStyles(name: string, personName: readonly string[], theme: Theme) {
-  return {
-    fontWeight:
-      personName.indexOf(name) === -1
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium,
-  };
-}
-
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
-
 export default function CreateDesignedProductForm(
   props: ICreateDesignedProductFormProps
 ) {
   const { handleCloseDialog } = props;
-  const theme = useTheme();
   const previews = useAppSelector((state) => state.previews);
   const blueprints = useAppSelector((state) => state.blueprintsData.blueprints);
   const [images, setImages] = React.useState<ImageListType>();
-  const colors = useAppSelector((state) => state.blueprintsData.colors);
-  console.log(colors, "colorrr");
   const router = useRouter();
 
   const defaultValues: FormAddDesignInfo = {
@@ -100,18 +78,8 @@ export default function CreateDesignedProductForm(
     defaultValues,
     resolver: yupResolver(schema),
   });
-  const [colorsList, setColorsList] = React.useState<string[]>([]);
-  const handleChange = (event: SelectChangeEvent<typeof colorsList>) => {
-    const {
-      target: { value },
-    } = event;
-    setColorsList(
-      // On autofill we get a stringified value.
-      typeof value === "string" ? value.split(",") : value
-    );
-  };
+  const colors = useAppSelector((state) => state.selectedColors);
 
-  const maxNumber = 69;
   const onChange = (imageList: ImageListType, addUpdateIndex: any) => {
     setImages(imageList);
     // data for submit
@@ -172,10 +140,7 @@ export default function CreateDesignedProductForm(
           <div className="card-body">
             <form onSubmit={handleSubmit(onSubmit)}>
               <div className="row mb-3">
-                <label
-                  className="col-sm-2 col-form-label"
-                  htmlFor="basic-icon-default-fullname"
-                >
+                <label htmlFor="basic-icon-default-fullname">
                   Tên thiết kế
                 </label>
 
@@ -198,67 +163,9 @@ export default function CreateDesignedProductForm(
                   )}
                 </div>
               </div>
-              <div className="row mb-3">
-                <label
-                  className="col-sm-2 col-form-label"
-                  htmlFor="basic-icon-default-fullname"
-                >
-                  Màu áo
-                </label>
-                <div className="col-sm-10">
-                  <FormControl sx={{ m: 1, width: 300, mt: 3 }}>
-                    <Select
-                      multiple
-                      displayEmpty
-                      value={colorsList}
-                      onChange={handleChange}
-                      variant="standard"
-                      renderValue={(selected) => {
-                        if (selected.length === 0) {
-                          return <em>{colors[0]}</em>;
-                        }
 
-                        return selected.map((color) => (
-                          // eslint-disable-next-line @next/next/no-img-element
-                          <img
-                            key={color}
-                            width={22}
-                            height={22}
-                            className="rounded-circle border"
-                            src={
-                              "https://images.printify.com/5853fec7ce46f30f8328200a"
-                            }
-                            style={{ backgroundColor: color }}
-                            alt={color}
-                          />
-                        ));
-                      }}
-                      MenuProps={MenuProps}
-                      inputProps={{ "aria-label": "Without label" }}
-                    >
-                      <MenuItem disabled value="">
-                        <em>{colors[0]}</em>
-                      </MenuItem>
-                      {colors.map((name) => (
-                        <MenuItem
-                          key={name}
-                          value={name}
-                          style={getStyles(name, colorsList, theme)}
-                        >
-                          {name}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                </div>
-              </div>
               <div className="row mb-3">
-                <label
-                  className="col-sm-2 col-form-label"
-                  htmlFor="basic-icon-default-company"
-                >
-                  Mô tả
-                </label>
+                <label htmlFor="basic-icon-default-company">Mô tả</label>
                 <div className="col-sm-10">
                   <div className="input-group input-group-merge">
                     <input
@@ -278,6 +185,7 @@ export default function CreateDesignedProductForm(
                   )}
                 </div>
               </div>
+              <SelectColor />
 
               <div className="d-flex justify-content-center">
                 <div className="col-sm-10 d-flex justify-content-around">
