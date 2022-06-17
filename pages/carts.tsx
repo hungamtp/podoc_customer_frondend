cd6d9825eb2a8a52a1e79cd0b0e9f40a31b667fe
Hung <hungamtp@gmail.com>
2022-06-18 03:26:23 +0700

    fix align in cart

--- pages/carts.tsx
+++ pages/carts.tsx
@@ -18,7 +18,7 @@ export default function Carts({}: Props) {
           <div className="row mt-5 justify-content-center">
             <div className="col-lg-12 text-center">
               <div className="pages-heading">
-                <h4 className="title mb-0"> Shopping Cart </h4>
+                <h2 className="title mb-0">Cart</h2>
               </div>
             </div>
           </div>
@@ -27,14 +27,9 @@ export default function Carts({}: Props) {
             <nav aria-label="breadcrumb" className="d-inline-block">
               <ul className="breadcrumb bg-white rounded shadow mb-0 px-4 py-2">
                 <li className="breadcrumb-item">
-                  <a href="index.html">Landrick</a>
+                  <a href="index.html">Print on demand</a>
                 </li>
-                <li className="breadcrumb-item">
-                  <a href="home">Shop</a>
-                </li>
-                <li className="breadcrumb-item active" aria-current="page">
-                  Cart
-                </li>
+                <li className="breadcrumb-item active">Cart</li>
               </ul>
             </nav>
           </div>
@@ -51,82 +46,82 @@ export default function Carts({}: Props) {
         <div className="container">
           <div className="row">
             <div className="col-12">
-              <div className="table-responsive bg-white shadow rounded"></div>
-            </div>
-          </div>
+              <div className="table-responsive bg-white shadow rounded">
+                {carts?.length == 0 ? (
+                  <div>Have no product in cart</div>
+                ) : (
+                  <table className="table mb-0 table-center">
+                    <thead>
+                      <tr>
+                        <th className="border-bottom py-3" style={{ minWidth: '20px' }}></th>
+                        <th className="border-bottom text-start py-3" style={{ minWidth: '200px' }}>
+                          Product
+                        </th>
+                        <th className="border-bottom text-center py-3" style={{ minWidth: '50px' }}>
+                          Size
+                        </th>
+                        <th className="border-bottom text-center py-3" style={{ minWidth: '50px' }}>
+                          Color
+                        </th>
+                        <th className="border-bottom text-center py-3" style={{ minWidth: '50px' }}>
+                          Price
+                        </th>
+                        <th className="border-bottom text-center py-3" style={{ minWidth: '100px' }}>
+                          Quantity
+                        </th>
+                        <th className="border-bottom text-end py-3 pe-4" style={{ minWidth: '100px' }}>
+                          Total
+                        </th>
+                      </tr>
+                    </thead>
 
-          {carts?.length == 0 ? (
-            <div>chuwa cos san pham trong cac</div>
-          ) : (
-            <table className="table mb-0 table-center">
-              <thead>
-                <tr>
-                  <th className="border-bottom py-3" style={{ minWidth: '20px' }}></th>
-                  <th className="border-bottom text-start py-3" style={{ minWidth: '300px' }}>
-                    Product
-                  </th>
-                  <th className="border-bottom text-center py-3" style={{ minWidth: '60px' }}>
-                    Size
-                  </th>
-                  <th className="border-bottom text-center py-3" style={{ minWidth: '60px' }}>
-                    Color
-                  </th>
-                  <th className="border-bottom text-center py-3" style={{ minWidth: '60px' }}>
-                    Price
-                  </th>
-                  <th className="border-bottom text-center py-3" style={{ minWidth: '160px' }}>
-                    Qty
-                  </th>
-                  <th className="border-bottom text-end py-3 pe-4" style={{ minWidth: '160px' }}>
-                    Total
-                  </th>
-                </tr>
-              </thead>
-
-              <tbody>
-                {carts?.map(cart => {
-                  return <Cart key={cart.id} cart={cart} />;
-                })}
-              </tbody>
-            </table>
-          )}
-          {haveProduct && (
-            <div className="row">
-              <div className="col-lg-8 col-md-6 mt-4 pt-2">
-                <a href="designs" className="btn btn-primary">
-                  Shop More
-                </a>
-                <a href=" " className="btn btn-soft-primary ms-2">
-                  Update Cart
-                </a>
-              </div>
-              <div className="col-lg-4 col-md-6 ms-auto mt-4 pt-2">
-                <div className="table-responsive bg-white rounded shadow">
-                  <table className="table table-center table-padding mb-0">
                     <tbody>
-                      <tr>
-                        <td className="h6 ps-4 py-3">Subtotal</td>
-                        <td className="text-end fw-bold pe-4">$ 2190</td>
-                      </tr>
-                      <tr>
-                        <td className="h6 ps-4 py-3">Taxes</td>
-                        <td className="text-end fw-bold pe-4">$ 219</td>
-                      </tr>
-                      <tr className="bg-light">
-                        <td className="h6 ps-4 py-3">Total</td>
-                        <td className="text-end fw-bold pe-4">$ 2409</td>
-                      </tr>
+                      {carts?.map(cart => {
+                        return <Cart key={cart.id} cart={cart} />;
+                      })}
                     </tbody>
                   </table>
-                </div>
-                <div className="mt-4 pt-2 text-end">
-                  <a href="checkout" className="btn btn-primary">
-                    Proceed to checkout
-                  </a>
-                </div>
+                )}
               </div>
+              {haveProduct && (
+                <div className="row">
+                  <div className="col-lg-8 col-md-6 mt-4 pt-2">
+                    <a href="designs" className="btn btn-primary">
+                      Shop More
+                    </a>
+                    <a href=" " className="btn btn-soft-primary ms-2">
+                      Update Cart
+                    </a>
+                  </div>
+                  <div className="col-lg-4 col-md-6 ms-auto mt-4 pt-2">
+                    <div className="table-responsive bg-white rounded shadow">
+                      <table className="table table-center table-padding mb-0">
+                        <tbody>
+                          <tr>
+                            <td className="h6 ps-4 py-3">Subtotal</td>
+                            <td className="text-end fw-bold pe-4">$ 2190</td>
+                          </tr>
+                          <tr>
+                            <td className="h6 ps-4 py-3">Taxes</td>
+                            <td className="text-end fw-bold pe-4">$ 219</td>
+                          </tr>
+                          <tr className="bg-light">
+                            <td className="h6 ps-4 py-3">Total</td>
+                            <td className="text-end fw-bold pe-4">$ 2409</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                    <div className="mt-4 pt-2 text-end">
+                      <a href="checkout" className="btn btn-primary">
+                        Proceed to checkout
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
-          )}
+          </div>
         </div>
       </section>
     </>
