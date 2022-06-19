8eb28677f12d0f3ca5c269c1b712390e66c88224
Sang <vuvietsang10a9@gmail.com>
2022-06-19 18:41:06 +0700

    add paypal lib

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
-import { EmptyLayout } from '@/components/layouts';
-import { AppPropsWithLayout } from '@/models/common';
-import { persistor, store } from '@/redux/store';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import '../styles/raw-product.css';
-import '../styles/product-detail.css';
-import { PersistGate } from 'redux-persist/integration/react';
-import { Provider } from 'react-redux';
-import '../styles/globals.css';
-import '../styles/global.scss';
+import { EmptyLayout } from "@/components/layouts";
+import { AppPropsWithLayout } from "@/models/common";
+import { persistor, store } from "@/redux/store";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import "../styles/raw-product.css";
+import "../styles/product-detail.css";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import "../styles/globals.css";
+import "../styles/global.scss";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const Layout = Component.Layout ?? EmptyLayout;
   const queryClient = new QueryClient();
@@ -16,9 +17,16 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <QueryClientProvider client={queryClient}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
+          <PayPalScriptProvider
+            options={{
+              "client-id":
+                "AXtc9DqyqTCgesK5RMtPckwA8VQ7r0zKWKMABjTFdZXrbIpMpFCz2BRMiFMM4W9b4gISymIZHp1kNnaK",
+            }}
+          >
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </PayPalScriptProvider>
         </QueryClientProvider>
       </PersistGate>
     </Provider>
