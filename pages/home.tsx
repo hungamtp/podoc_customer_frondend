ea5a32779adb72803e7d354f8768fae31d1ec73f
duynk-hub <duyuminh41@gmail.com>
2022-07-10 14:01:14 +0700

    update ui

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,18 +1,24 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react';
-import { MainLayout } from '@/components/layouts';
-import { useGetHighestRateDesign } from '@/hooks/api/use-get-highest-rate-design';
-import DesignedProducts from '@/components/common/designed-products';
-import { useAppDispatch, useAppSelector } from '@/components/hooks/reduxHook';
+import React from "react";
+import { MainLayout } from "@/components/layouts";
+import { useGetHighestRateDesign } from "@/hooks/api/use-get-highest-rate-design";
+import DesignedProducts from "@/components/common/designed-products";
+import { useAppDispatch, useAppSelector } from "@/components/hooks/reduxHook";
+import Link from "next/link";
 type Props = {};
 
 export default function HomePage({}: Props) {
-  const { data: response, isLoading: isLoadingAccount } = useGetHighestRateDesign();
+  const { data: response, isLoading: isLoadingAccount } =
+    useGetHighestRateDesign();
   const dispatch = useAppDispatch();
   return (
     <>
       <section className="home-slider position-relative">
-        <div id="carouselExampleInterval" className="carousel slide carousel-fade" data-bs-ride="carousel">
+        <div
+          id="carouselExampleInterval"
+          className="carousel slide carousel-fade"
+          data-bs-ride="carousel"
+        >
           <div className="carousel-indicators">
             <button
               type="button"
@@ -22,15 +28,27 @@ export default function HomePage({}: Props) {
               aria-current="true"
               aria-label="Slide 1"
             ></button>
-            <button type="button" data-bs-target="#carouselExampleInterval" data-bs-slide-to="1" aria-label="Slide 2"></button>
-            <button type="button" data-bs-target="#carouselExampleInterval" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleInterval"
+              data-bs-slide-to="1"
+              aria-label="Slide 2"
+            ></button>
+            <button
+              type="button"
+              data-bs-target="#carouselExampleInterval"
+              data-bs-slide-to="2"
+              aria-label="Slide 3"
+            ></button>
           </div>
 
           <div className="carousel-inner">
             <div className="carousel-item active" data-bs-interval="3000">
               <div
                 className="bg-home slider-rtl-2 d-flex align-items-center"
-                style={{ background: "url('asset/images/shop/bg2.jpg') center center" }}
+                style={{
+                  background: "url('asset/images/shop/bg2.jpg') center center",
+                }}
               >
                 <div className="bg-overlay bg-overlay-white opacity-5"></div>
                 <div className="container">
@@ -38,17 +56,18 @@ export default function HomePage({}: Props) {
                     <div className="col-lg-7 col-md-7">
                       <div className="title-heading mt-4">
                         <h1 className="display-4 title-white fw-bold mb-3">
-                          New Accessories <br />
-                          Collections
+                          Print On Demand
                         </h1>
                         <p className="para-desc text-dark title-white">
-                          Launch your campaign and benefit from our expertise on designing and managing conversion centered bootstrap v5
-                          html page.
+                          In theo yêu cầu là một công nghệ in và quy trình kinh
+                          doanh trong đó các bản sao sách không được in cho đến
+                          khi công ty nhận được một đơn đặt hàng, cho phép in số
+                          lượng đơn hoặc số lượng nhỏ..
                         </p>
                         <div className="mt-4">
-                          <a href=" " className="btn btn-soft-primary">
-                            Shop Now
-                          </a>
+                          <Link href="\designs">
+                            <a className="btn btn-soft-primary">Mua ngay</a>
+                          </Link>
                         </div>
                       </div>
                     </div>
@@ -59,8 +78,11 @@ export default function HomePage({}: Props) {
 
             <div className="carousel-item" data-bs-interval="3000">
               <div
-                className="bg-home slider-rtl-1 d-flex align-items-center"
-                style={{ background: "url('asset/images/shop/bg1.jpg') center center" }}
+                className="bg-home slider-rtl-1 d-flex align-items-center "
+                style={{
+                  background: "url('asset/images/shop/hp-2.png') center center",
+                  backgroundSize: "1500px 850px",
+                }}
               >
                 <div className="bg-overlay bg-overlay-white opacity-5"></div>
                 <div className="container">
@@ -68,17 +90,18 @@ export default function HomePage({}: Props) {
                     <div className="col-lg-7 col-md-7">
                       <div className="title-heading mt-4">
                         <h1 className="display-4 title-white fw-bold mb-3">
-                          Headphones <br />
-                          Speaker
+                          Kiếm tiền <br />
+                          Online
                         </h1>
                         <p className="para-desc text-dark title-white">
-                          Launch your campaign and benefit from our expertise on designing and managing conversion centered bootstrap v5
-                          html page.
+                          Tại PODOC khách hàng có thể tự đăng bán các mẫu thiết
+                          kế của bản thân và thu lợi nhuận từ chính các mẫu
+                          thiết kế đó.
                         </p>
                         <div className="mt-4">
-                          <a href=" " className="btn btn-soft-primary">
-                            Shop Now
-                          </a>
+                          <Link href="\designs">
+                            <a className="btn btn-soft-primary">Mua ngay</a>
+                          </Link>
                         </div>
                       </div>
                     </div>
@@ -90,25 +113,28 @@ export default function HomePage({}: Props) {
             <div className="carousel-item" data-bs-interval="3000">
               <div
                 className="bg-home slider-rtl-3 d-flex align-items-center"
-                style={{ background: "url('asset/images/shop/bg3.jpg') center center" }}
+                style={{
+                  background: "url('asset/images/shop/hp-3.jpg') center center",
+                  backgroundSize: "1500px 850px",
+                }}
               >
                 <div className="bg-overlay bg-overlay-white opacity-5"></div>
                 <div className="container">
                   <div className="row align-items-center mt-5">
                     <div className="col-lg-7 col-md-7">
                       <div className="title-heading mt-4">
-                        <h1 className="display-4 title-white fw-bold mb-3">
-                          Modern Furniture, <br />
-                          Armchair
-                        </h1>
+                        <h4 className="display-4 title-white fw-bold mb-3">
+                          Dễ dàng thêm thiết <br /> kế vào sản phẩm
+                        </h4>
                         <p className="para-desc text-dark title-white">
-                          Launch your campaign and benefit from our expertise on designing and managing conversion centered bootstrap v5
-                          html page.
+                          Với các công cụ thiết kế miễn phí của chúng tôi, bạn
+                          có thể dễ dàng thêm các thiết kế tùy chỉnh của mình
+                          vào sản phẩm của bạn.
                         </p>
                         <div className="mt-4">
-                          <a href=" " className="btn btn-soft-primary">
-                            Shop Now
-                          </a>
+                          <Link href="\designs">
+                            <a className="btn btn-soft-primary">Mua ngay</a>
+                          </Link>
                         </div>
                       </div>
                     </div>
@@ -120,7 +146,12 @@ export default function HomePage({}: Props) {
         </div>
       </section>
 
-      {response && <DesignedProducts title="Highest Rate Designed Products" data={response} />}
+      {response && (
+        <DesignedProducts
+          title="Highest Rate Designed Products"
+          data={response}
+        />
+      )}
       {response && <DesignedProducts title="Best Seller" data={response} />}
 
       <div className="container mt-100 mt-60">
@@ -134,7 +165,11 @@ export default function HomePage({}: Props) {
           <div className="col-lg-2 col-md-4 col-6 mt-4 pt-2">
             <div className="card explore-feature border-0 rounded text-center bg-white">
               <div className="card-body">
-                <img src="asset/images/shop/categories/fashion.jpg" className="avatar avatar-small rounded-circle shadow-md" alt="" />
+                <img
+                  src="asset/images/shop/categories/fashion.jpg"
+                  className="avatar avatar-small rounded-circle shadow-md"
+                  alt=""
+                />
                 <div className="content mt-3">
                   <h6 className="mb-0">
                     <a href=" " className="title text-dark">
@@ -149,7 +184,11 @@ export default function HomePage({}: Props) {
           <div className="col-lg-2 col-md-4 col-6 mt-4 pt-2">
             <div className="card explore-feature border-0 rounded text-center bg-white">
               <div className="card-body">
-                <img src="asset/images/shop/categories/sports.jpg" className="avatar avatar-small rounded-circle shadow-md" alt="" />
+                <img
+                  src="asset/images/shop/categories/sports.jpg"
+                  className="avatar avatar-small rounded-circle shadow-md"
+                  alt=""
+                />
                 <div className="content mt-3">
                   <h6 className="mb-0">
                     <a href=" " className="title text-dark">
@@ -164,7 +203,11 @@ export default function HomePage({}: Props) {
           <div className="col-lg-2 col-md-4 col-6 mt-4 pt-2">
             <div className="card explore-feature border-0 rounded text-center bg-white">
               <div className="card-body">
-                <img src="asset/images/shop/categories/music.jpg" className="avatar avatar-small rounded-circle shadow-md" alt="" />
+                <img
+                  src="asset/images/shop/categories/music.jpg"
+                  className="avatar avatar-small rounded-circle shadow-md"
+                  alt=""
+                />
                 <div className="content mt-3">
                   <h6 className="mb-0">
                     <a href=" " className="title text-dark">
@@ -179,7 +222,11 @@ export default function HomePage({}: Props) {
           <div className="col-lg-2 col-md-4 col-6 mt-4 pt-2">
             <div className="card explore-feature border-0 rounded text-center bg-white">
               <div className="card-body">
-                <img src="asset/images/shop/categories/furniture.jpg" className="avatar avatar-small rounded-circle shadow-md" alt="" />
+                <img
+                  src="asset/images/shop/categories/furniture.jpg"
+                  className="avatar avatar-small rounded-circle shadow-md"
+                  alt=""
+                />
                 <div className="content mt-3">
                   <h6 className="mb-0">
                     <a href=" " className="title text-dark">
@@ -194,7 +241,11 @@ export default function HomePage({}: Props) {
           <div className="col-lg-2 col-md-4 col-6 mt-4 pt-2">
             <div className="card explore-feature border-0 rounded text-center bg-white">
               <div className="card-body">
-                <img src="asset/images/shop/categories/electronics.jpg" className="avatar avatar-small rounded-circle shadow-md" alt="" />
+                <img
+                  src="asset/images/shop/categories/electronics.jpg"
+                  className="avatar avatar-small rounded-circle shadow-md"
+                  alt=""
+                />
                 <div className="content mt-3">
                   <h6 className="mb-0">
                     <a href=" " className="title text-dark">
@@ -209,7 +260,11 @@ export default function HomePage({}: Props) {
           <div className="col-lg-2 col-md-4 col-6 mt-4 pt-2">
             <div className="card explore-feature border-0 rounded text-center bg-white">
               <div className="card-body">
-                <img src="asset/images/shop/categories/mobile.jpg" className="avatar avatar-small rounded-circle shadow-md" alt="" />
+                <img
+                  src="asset/images/shop/categories/mobile.jpg"
+                  className="avatar avatar-small rounded-circle shadow-md"
+                  alt=""
+                />
                 <div className="content mt-3">
                   <h6 className="mb-0">
                     <a href=" " className="title text-dark">
@@ -223,31 +278,6 @@ export default function HomePage({}: Props) {
         </div>
       </div>
       {/* End of season */}
-      <div className="container-fluid mt-100 mt-60">
-        <div className="rounded py-5" style={{ background: "url('asset/images/shop/cta.jpg') fixed" }}>
-          <div className="container">
-            <div className="row">
-              <div className="col-12">
-                <div className="section-title">
-                  <h2 className="fw-bold title-white mb-4">
-                    End of Season Clearance <br />
-                    Sale upto 30%
-                  </h2>
-                  <p className="para-desc para-white text-muted mb-0">
-                    Launch your campaign and benefit from our expertise on designing and managing conversion centered bootstrap v5 html
-                    page.
-                  </p>
-                  <div className="mt-4">
-                    <a href=" " className="btn btn-primary">
-                      Shop Now
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </>
   );
 }
