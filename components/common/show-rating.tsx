f329d7978ace1327b76155d89fda9e0e0b40a529
Hung <hungamtp@gmail.com>
2022-07-30 14:36:53 +0700

    fix rating

--- components/common/show-rating.tsx
+++ components/common/show-rating.tsx
@@ -1,5 +1,5 @@
-import { Rating } from "@mui/material";
-import * as React from "react";
+import { Rating } from '@mui/material';
+import * as React from 'react';
 
 export interface IShowRatingProps {
   rate: number;
@@ -10,20 +10,14 @@ export default function ShowRating({ rate, rateCount }: IShowRatingProps) {
   return (
     <div>
       <div className="d-flex justify-content-between">
-        <div className="d-flex">
-          <div className="list-unstyled text-warning  ">{rate.toFixed(2)}</div>
-
-          <Rating
-            name="half-rating"
-            value={rate}
-            size="medium"
-            sx={{ marginY: "auto" }}
-            precision={0.5}
-            readOnly
-          />
+        <div className="d-flex" style={{ justifyContent: 'space-between' }}>
+          <div>
+            <Rating name="half-rating" value={rate} size="medium" sx={{ marginY: 'auto' }} precision={0.5} readOnly />
+          </div>
+          <div className="list-unstyled text-warning  ">({rate.toFixed(2)})</div>
         </div>
       </div>
-      <span className="sold-number ">({`trên ${rateCount} đánh giá`})</span>
+      <span className="sold-number ">{`Đánh giá : ${rateCount} `}</span>
     </div>
   );
 }
