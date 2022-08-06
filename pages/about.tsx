a7e0a98ae251364dbbeef27bd4526ae63c5b5842
Hung <hungamtp@gmail.com>
2022-08-06 17:24:13 +0700

    fix banner

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,14 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
-import { MainLayout } from "@/components/layouts";
-import Link from "next/link";
-import React from "react";
+import { MainLayout } from '@/components/layouts';
+import Link from 'next/link';
+import React from 'react';
 
 type Props = {};
 
 export default function About({}: Props) {
   return (
     <>
-      <section className="bg-half-170 bg-light d-table w-100">
+      <section
+        className="bg-half-170 bg-light d-table w-100"
+        style={{
+          background: "url('/asset/images/banner/banner.jpg') no-repeat center center fixed ",
+          marginTop: '70px',
+        }}
+      >
         <div className="container">
           <div className="row mt-5 justify-content-center">
             <div className="col-lg-12 text-center">
@@ -36,15 +42,8 @@ export default function About({}: Props) {
       </section>
       <div className="position-relative">
         <div className="shape overflow-hidden text-white">
-          <svg
-            viewBox="0 0 2880 48"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z"
-              fill="currentColor"
-            ></path>
+          <svg viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z" fill="currentColor"></path>
           </svg>
         </div>
       </div>
@@ -55,25 +54,20 @@ export default function About({}: Props) {
             <div className="col-lg-9">
               <div className="section-title">
                 <h5 className="title mb-4  text-center text-uppercase ">
-                  Chất lượng của sản phẩm và độ hài lòng của khách hàng là trên
-                  hết
+                  Chất lượng của sản phẩm và độ hài lòng của khách hàng là trên hết
                 </h5>
                 <p className="text-wrap fs-6 mb-0">
-                  Chọn cho mình slogan “
-                  <strong>Let your creativity on your T-shirt</strong>”, PODOC
-                  mong muốn đem đến những thiết kế không chỉ có tính thẩm mỹ
-                  cao, mà còn đầy ý nghĩa, chứa đựng những kỷ niệm, kí ức đẹp,
-                  lưu giữ trên từng chiếc áo của khách hàng.
+                  Chọn cho mình slogan “<strong>Let your creativity on your T-shirt</strong>”, PODOC mong muốn đem đến những thiết kế không
+                  chỉ có tính thẩm mỹ cao, mà còn đầy ý nghĩa, chứa đựng những kỷ niệm, kí ức đẹp, lưu giữ trên từng chiếc áo của khách
+                  hàng.
                   <br />
                   <br />
-                  Chúng tôi luôn đặt chất lượng vải và chất lượng hình in lên
-                  hàng đầu, để chiếc áo sẽ giống như những kỷ niệm: luôn luôn
+                  Chúng tôi luôn đặt chất lượng vải và chất lượng hình in lên hàng đầu, để chiếc áo sẽ giống như những kỷ niệm: luôn luôn
                   bền lâu.
                   <br />
                   <br />
-                  PODOC sẽ không ngừng nâng cao dịch vụ, phấn đấu để trở thành
-                  thương hiệu hàng đầu về chăm sóc khách hàng, thiết kế độc đáo,
-                  sản phẩm chất lượng tại Việt Nam.
+                  PODOC sẽ không ngừng nâng cao dịch vụ, phấn đấu để trở thành thương hiệu hàng đầu về chăm sóc khách hàng, thiết kế độc
+                  đáo, sản phẩm chất lượng tại Việt Nam.
                 </p>
               </div>
             </div>
@@ -104,10 +98,7 @@ export default function About({}: Props) {
 
                 <div className="card-body">
                   <h5 className="text-dark">Giao hàng nhanh</h5>
-                  <p className="text-muted mb-0">
-                    Các nhà máy sẽ hỗ trợ giao sản phẩm đến tận tay các khách
-                    hàng một cách nhanh chóng
-                  </p>
+                  <p className="text-muted mb-0">Các nhà máy sẽ hỗ trợ giao sản phẩm đến tận tay các khách hàng một cách nhanh chóng</p>
                 </div>
               </div>
             </div>
@@ -121,8 +112,7 @@ export default function About({}: Props) {
                 <div className="card-body">
                   <h5 className="text-dark">Hỗ trợ 24/7</h5>
                   <p className="text-muted mb-0">
-                    Nhân viên của PODOC sẵn sàng hỗ trợ các khách hàng giải
-                    quyết vấn đề nhiệt tình và hiệu quả
+                    Nhân viên của PODOC sẵn sàng hỗ trợ các khách hàng giải quyết vấn đề nhiệt tình và hiệu quả
                   </p>
                 </div>
               </div>
@@ -136,10 +126,7 @@ export default function About({}: Props) {
 
                 <div className="card-body">
                   <h5 className="text-dark">Thanh toán online</h5>
-                  <p className="text-muted mb-0">
-                    PODOC cung cấp cho khách hàng hình thức thanh toán online
-                    cực kì thuận tiện trên MOMO
-                  </p>
+                  <p className="text-muted mb-0">PODOC cung cấp cho khách hàng hình thức thanh toán online cực kì thuận tiện trên MOMO</p>
                 </div>
               </div>
             </div>
