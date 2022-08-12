f90834af74adc01ba5fda358a33bcdc22cb9a334
studingHtml <hieuthomnghiep@gmail.com>
2022-08-12 22:54:44 +0700

    update my-order

--- components/design/custom-auto-complete-selecte.tsx
+++ components/design/custom-auto-complete-selecte.tsx
@@ -1,17 +1,25 @@
 export interface IAutoCompleteSelectProps {
   list: { label: string; value: string }[];
   handleChange: (data: any) => void;
+  data: string;
 }
 
-export default function AutoCompleteSelect(props: IAutoCompleteSelectProps) {
-  const { list, handleChange } = props;
+export default function CustomAutoCompleteSelect(
+  props: IAutoCompleteSelectProps
+) {
+  const { list, handleChange, data } = props;
   const onChange = (e: any) => {
     const value = e.target[e.target.selectedIndex].getAttribute("value");
     handleChange(value);
   };
+
   return (
     <div>
-      <select className="form-select shadow w-full " onChange={onChange}>
+      <select
+        className="form-select shadow w-full "
+        onChange={onChange}
+        value={data}
+      >
         {list.map((data) => (
           <option key={data.label} value={data.value}>
             {data.label}
--- pages/account/myorders.tsx
+++ pages/account/myorders.tsx
@@ -3,17 +3,65 @@ import MyOrdersTable from "@/components/common/my-orders-table";
 import { Account, MainLayout } from "@/components/layouts";
 import useMyOrders from "@/hooks/api/order/use-my-orders";
 import { Filter } from "@/services/order";
-import React from "react";
+import { MyOrdersDto } from "@/services/order/dto";
+import React, { useEffect, useState } from "react";
 
 type Props = {};
+
+const criteriaEnum = {
+  ALL: "all",
+  CANCELED: "cancel",
+  UNPAID: "unpaid",
+  PENDING: "pending",
+};
 
 export default function MyOrders() {
   const [filter, setFilter] = React.useState<Filter>({
     pageNumber: 0,
     pageSize: 4,
   });
+  const [criteria, setCriteria] = useState(criteriaEnum.ALL);
+  console.log(filter, "filter");
 
   const { data: myOrdersResponse, isLoading: isLoading } = useMyOrders(filter);
+
+  const handleFilter = (criteria: string) => {
+    if (criteria === criteriaEnum.PENDING) {
+      setFilter({
+        pageNumber: 0,
+        pageSize: 4,
+        cancel: false,
+        paid: true,
+      });
+      setCriteria(criteriaEnum.PENDING);
+    } else if (criteria === criteriaEnum.CANCELED) {
+      setFilter((filter) => {
+        return {
+          pageSize: 4,
+          pageNumber: filter.pageNumber,
+          cancel: true,
+        };
+      });
+      setCriteria(criteriaEnum.CANCELED);
+    } else if (criteria === criteriaEnum.UNPAID) {
+      setFilter((filter) => {
+        return {
+          pageSize: 4,
+          pageNumber: filter.pageNumber,
+          paid: false,
+        };
+      });
+      setCriteria(criteriaEnum.UNPAID);
+    } else if (criteria === criteriaEnum.ALL) {
+      setFilter((filter) => {
+        return {
+          pageSize: 4,
+          pageNumber: filter.pageNumber,
+        };
+      });
+      setCriteria(criteriaEnum.ALL);
+    }
+  };
 
   return (
     <>
@@ -21,8 +69,10 @@ export default function MyOrders() {
         <MyOrdersTable
           filter={filter}
           setFilter={setFilter}
-          myOrdersResponse={myOrdersResponse}
+          handleFilter={handleFilter}
           isLoading={isLoading}
+          criteria={criteria}
+          myOrdersResponse={myOrdersResponse}
         />
       )}
     </>
