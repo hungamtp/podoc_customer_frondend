e6f2c15571555ea681015d5eee95b43eaab93476
Sang Vu <vuvietsang10a9@gmail.com>
2022-08-16 21:57:16 +0700

    edit footer

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,105 +1,23 @@
 /* eslint-disable react/jsx-no-target-blank */
-/* eslint-disable @next/next/no-img-element */
-import useCategory from "@/hooks/api/use-category";
-import Link from "next/link";
-import React from "react";
-import CartIcon from "../../icons/cart-icon";
-import EyeIcon from "../../icons/eye-icon";
-import HeaerIcon from "../../icons/heart-icon";
 
 type Props = {};
 
 export default function Footer({}: Props) {
-  const { data: categories, isLoading: isCategoryLoading } = useCategory();
 
   return (
     <footer className="footer">
       <div className="container">
-        <div className="row">
-          <div className="col-12">
             <div className="footer-py-60">
-              <div className="row">
-                <div className="col-lg-3 col-12 mb-0 mb-md-4 pb-0 pb-md-2">
+              <div className="d-flex justify-content-center">
+                      <h5 className="footer-head mb-0">Shopping & Clothes</h5>
+              </div>
+              <div className="d-flex justify-content-center">
                   <p className="mt-4">
                     PODOC sẽ mang đến cho bạn một trải nghiệm tốt nhất.
                   </p>
-                </div>
-
-                <div className="col-lg-6">
-                  <div className="row">
-                    <div className="col-12 mb-4 pb-2">
-                      <h5 className="footer-head mb-0">Shopping & Clothes</h5>
-                    </div>
-
-                    <div className="col-lg-4 col-md-4 col-12">
-                      <ul className="list-unstyled footer-list">
-                        {!isCategoryLoading &&
-                          categories &&
-                          categories.slice(0, 6).map((category) => (
-                            <li key={category.id}>
-                              <a href="#" className="text-foot">
-                                <i className="uil uil-angle-right-b me-1"></i>{" "}
-                                {category.name}
-                              </a>
-                            </li>
-                          ))}
-                      </ul>
-                    </div>
-
-                    {!isCategoryLoading &&
-                      categories?.slice(6, 13).length != 0 &&
-                      categories?.slice(6, 13).map((category) => (
-                        <div
-                          key={category.id}
-                          className="col-lg-4 col-md-4 col-12 mt-2 mt-sm-0"
-                        >
-                          <ul className="list-unstyled footer-list">
-                            <li>
-                              <a href="#" className="text-foot">
-                                <i className="uil uil-angle-right-b me-1"></i>{" "}
-                                {category.name}
-                              </a>
-                            </li>
-                          </ul>
-                        </div>
-                      ))}
-
-                    <div className="col-lg-4 col-md-4 col-12 mt-2 mt-sm-0">
-                      <ul className="list-unstyled footer-list">
-                        <li>
-                          <Link href="/account" className="text-foot">
-                            <a>
-                              <i className="uil uil-angle-right-b me-1"></i>Tài
-                              khoản{" "}
-                            </a>
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href="/account/myorders" className="text-foot">
-                            <a>
-                              <i className="uil uil-angle-right-b me-1"></i>Lịch
-                              sử mua hàng{" "}
-                            </a>
-                          </Link>
-                        </li>
-                        <li>
-                          <Link href="/account/mydesign" className="text-foot">
-                            <a>
-                              <i className="uil uil-angle-right-b me-1"></i>
-                              Thiết kế của tôi{" "}
-                            </a>
-                          </Link>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </div>
             </div>
-          </div>
         </div>
       </div>
-
       <div className="container">
         <div className="row">
           <div className="col-12">
