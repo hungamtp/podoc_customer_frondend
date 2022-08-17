39f10e347392d977a70e9022781b883f316e8f59
duynk-hub <duyuminh41@gmail.com>
2022-08-17 14:43:34 +0700

    update success

--- hooks/api/account/use-reset-password.ts
+++ hooks/api/account/use-reset-password.ts
@@ -1,23 +1,21 @@
-import { ResetPasswordDto } from '@/services/account/dto';
-import { resetPassword } from '@/services/account/index';
-import { useRouter } from 'next/router';
-import { useMutation, useQueryClient } from 'react-query';
-
+import { ResetPasswordDto } from "@/services/account/dto";
+import { resetPassword } from "@/services/account/index";
+import { useRouter } from "next/router";
+import { useMutation, useQueryClient } from "react-query";
 
 const useResetPassword = () => {
-    const queryClient = useQueryClient();
-    const router = useRouter();
-	return useMutation(
-		      
-        async (data: ResetPasswordDto) => {
-            return await resetPassword(data);
-		},
-		{
-			onSuccess: () => {
-                router.push("/");
-			},
-		}
-	);
+  const queryClient = useQueryClient();
+  const router = useRouter();
+  return useMutation(
+    async (data: ResetPasswordDto) => {
+      return await resetPassword(data);
+    },
+    {
+      onSuccess: () => {
+        router.push("/auth/reset-password-success-form");
+      },
+    }
+  );
 };
 
 export default useResetPassword;
